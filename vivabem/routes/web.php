6fe3a8297da16b4c1c860f9d5048cb33b3e7bfd4
GustaSoier <gsampaiosoier0@gmail.com>
2023-11-11 20:35:46 -0300

    Academia Viva Bem
    Criando a rota de contato

--- vivabem/routes/web.php
+++ vivabem/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SobreController;
 use Illuminate\Support\Facades\Route;
@@ -21,3 +22,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/sobre', [SobreController::class, 'index'])->name('sobre');
+Route::get('/contato', [ContatoController::class, 'index'])->name('contato');
+
