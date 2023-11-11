a53571e07d16a37fb89760523b5650eaed26ea66
GustaSoier <gsampaiosoier0@gmail.com>
2023-11-11 20:41:35 -0300

    Academia Viva Bem
    Criando a rota de modalidades

--- vivabem/routes/web.php
+++ vivabem/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ModalidadesController;
 use App\Http\Controllers\NoticiasController;
 use App\Http\Controllers\SobreController;
 use Illuminate\Support\Facades\Route;
@@ -24,5 +25,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/sobre', [SobreController::class, 'index'])->name('sobre');
 Route::get('/contato', [ContatoController::class, 'index'])->name('contato');
-Route::get('/noticias', [NoticiasController::class, 'index'])->name('noticia');
+Route::get('/noticia', [NoticiasController::class, 'index'])->name('noticia');
+Route::get('/modalidade', [ModalidadesController::class, 'index'])->name('modalidade')
 
