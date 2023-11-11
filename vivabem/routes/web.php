8af7b37e5c1f035095782f67a4310d3429eb88c9
GustaSoier <gsampaiosoier0@gmail.com>
2023-11-11 20:39:55 -0300

    Academia Viva Bem
    Criando a rota de noticias

--- vivabem/routes/web.php
+++ vivabem/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NoticiasController;
 use App\Http\Controllers\SobreController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,4 +24,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/sobre', [SobreController::class, 'index'])->name('sobre');
 Route::get('/contato', [ContatoController::class, 'index'])->name('contato');
+Route::get('/noticias', [NoticiasController::class, 'index'])->name('noticia');
 
