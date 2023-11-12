4bc9e1c633e97946929651801580ba6020309820
GustaSoier <gsampaiosoier0@gmail.com>
2023-11-12 02:24:15 -0300

    Academia Viva Bem
    Troquei o caminho de "site.noticias" para "site.noticia", pois estava dando interferência com o nome da pasta.

--- vivabem/app/Http/Controllers/NoticiasController.php
+++ vivabem/app/Http/Controllers/NoticiasController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class NoticiasController extends Controller
 {
     public function index(){
-        return view('site.noticias');
+        return view('site.noticia');
     }
 }
