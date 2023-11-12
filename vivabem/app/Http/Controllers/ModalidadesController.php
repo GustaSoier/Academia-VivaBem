7192d1c8ce858be47d5e914789a0a18c36d1f22c
GustaSoier <gsampaiosoier0@gmail.com>
2023-11-12 02:28:00 -0300

    Academia Viva Bem
    Troquei o caminho de "site.modalidades" para "site.modalidade", pois estava dando interferência com o nome da pasta.

--- vivabem/app/Http/Controllers/ModalidadesController.php
+++ vivabem/app/Http/Controllers/ModalidadesController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class ModalidadesController extends Controller
 {
     public function index(){
-        return view('site.modalidades');
+        return view('site.modalidade');
     }
 }
